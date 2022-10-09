b33b2f2482e85361181e37484339a35cf7ee899b
jyuch <jyuch@users.noreply.github.com>
2022-10-09 20:25:51 +0900

    型名をデータ型から取得するように変更

--- tostring/src/main.rs
+++ tostring/src/main.rs
@@ -3,9 +3,15 @@ use tostring_macro::ToString;
 #[derive(ToString)]
 pub struct Struct {}
 
+#[derive(ToString)]
+pub struct Hoge {}
+
 fn main() {
     let a = Struct {};
     println!("{:?}", a);
+
+    let b = Hoge {};
+    println!("{:?}", b)
 }
 
 // Print macro expanded result
--- tostring_macro_internals/src/lib.rs
+++ tostring_macro_internals/src/lib.rs
@@ -1,11 +1,20 @@
 use proc_macro2::TokenStream;
 use quote::quote;
+use syn::{DeriveInput};
 
-pub fn derive(_input: TokenStream) -> TokenStream {
+pub fn derive(input: TokenStream) -> TokenStream {
+    // parse_macro_input! は proc_macro2 配下の TokenStream には使えないので
+    // syn::parse2(input) でパースする必要がある
+    let input: DeriveInput = syn::parse2(input).unwrap();
+
+    let src_ident = input.ident;
+    let src_ident_str = src_ident.to_string();
+
     (quote! {
-        impl ::std::fmt::Debug for Struct {
+        impl ::std::fmt::Debug for #src_ident {
             fn fmt(&self, formatter: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-                formatter.write_str("Piyo")
+                formatter.debug_struct( #src_ident_str )
+                    .finish()
             }
         }
     }).into()
