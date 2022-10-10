f5e60e045ae09be4a04542167a50ca8aed2bcf03
jyuch <jyuch@users.noreply.github.com>
2022-10-10 15:52:16 +0900

    コメントを追記

--- tostring_macro/src/lib.rs
+++ tostring_macro/src/lib.rs
@@ -2,5 +2,7 @@ use proc_macro::TokenStream;
 
 #[proc_macro_derive(ToString)]
 pub fn derive(input: TokenStream) -> TokenStream {
+    // マクロの実装を呼び出すだけ
+    // proc_macro から proc_macro2 の TokenStream に変換する
     tostring_macro_internals::derive(input.into()).into()
 }
